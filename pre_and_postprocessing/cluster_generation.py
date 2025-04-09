f30d92c909d64973c07d9838e063726eef5be4f5
Simone Rizzo <simone.rizzo@e4company.com>
2025-04-09 22:54:13 +0200

    Generate medium size dataset

--- pre_and_postprocessing/cluster_generation.py
+++ pre_and_postprocessing/cluster_generation.py
@@ -3,11 +3,11 @@ import pandas as pd
 from sklearn.datasets import make_blobs
 
 # Generate sample cluster points
-n_samples = 500000  # Total number of points
+n_samples = 80000  # Total number of points
 n_features = 2   # Number of features (dimensions)
-n_clusters = 6   # Number of clusters
+n_clusters = 8   # Number of clusters
 
-X, y = make_blobs(n_samples=n_samples, centers=n_clusters, n_features=n_features, random_state=42)
+X, y = make_blobs(n_samples=n_samples, centers=n_clusters, n_features=n_features, random_state=0x1337, center_box=(-20.0, 20.0))
 
 #print("y = ", y)
 
@@ -19,7 +19,7 @@ df = pd.DataFrame(
      'Cluster': y
     })
 
-df.to_csv('cluster_points_simple.csv', index=False)
+df.to_csv('cluster_points_80k.csv', index=False)
 
 # Plot the generated clusters
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap='viridis', alpha=0.6)
